d3b64cc617fb297bc834ab5c82f697aafffdd2c4
shailijha <shaili_jha@yahoo.co.in>
2020-03-02 15:56:43 -0500

    Added dummy article data and displayed in Blog page

--- src/app/articles/article-homepage/article-homepage.component.ts
+++ src/app/articles/article-homepage/article-homepage.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ARTICLES } from "../dummy-articles";
+import { Article } from "../article";
 
 @Component({
-  selector: 'app-article-homepage',
-  templateUrl: './article-homepage.component.html',
-  styleUrls: ['./article-homepage.component.scss']
+  selector: "app-article-homepage",
+  templateUrl: "./article-homepage.component.html",
+  styleUrls: ["./article-homepage.component.scss"]
 })
 export class ArticleHomepageComponent implements OnInit {
+  dummy_articles: Article[] = ARTICLES;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
