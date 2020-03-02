3389df2758e1a41fec2fb439ff5bd16a001f2400
InheritedSpark <am844@njit.edu>
2020-03-02 17:05:26 -0500

    fixed accounts module.ts imports

--- src/app/accounts/accounts.module.ts
+++ src/app/accounts/accounts.module.ts
@@ -4,11 +4,6 @@ import { CommonModule } from '@angular/common';
 import { AccountsRoutingModule } from './accounts-routing.module';
 import { MaterialModule } from '../material/material.module';
 import { SignUpComponent } from './sign-up/sign-up.component';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSelectModule } from '@angular/material/select';
-import { MatRadioModule } from '@angular/material/radio';
-import { MatCardModule } from '@angular/material/card';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
@@ -21,7 +16,6 @@ import { HttpClientModule } from '@angular/common/http';
     MaterialModule,
     ReactiveFormsModule,
     HttpClientModule,
-    MaterialModule,
   ],
   exports: [SignUpComponent]
 })
