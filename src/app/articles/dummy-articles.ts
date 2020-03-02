fb2086e0de1702074eefc6c60240ffc38bf38bed
shailijha <shaili_jha@yahoo.co.in>
2020-03-02 16:10:45 -0500

    final commit for 3/2

--- src/app/articles/dummy-articles.ts
+++ src/app/articles/dummy-articles.ts
@@ -19,7 +19,7 @@ export const ARTICLES: Article[] = [
     publish_date: new Date(),
     title: "Lorem 2",
     content:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas mi nisl, pharetra sit amet nulla nec, lobortis auctor nisl. Proin vestibulum urna nisl, non ultrices nisl malesuada ac. Suspendisse laoreet libero eu neque tristique, eu hendrerit nisl dapibus. Vestibulum aliquet nisl diam, vitae commodo magna egestas dapibus. Aliquam bibendum urna eros, ut tincidunt metus ultrices vitae. Ut pulvinar diam in ornare sagittis. Nullam ut elementum ex. Quisque vehicula dolor ac risus elementum placerat. In hac habitasse platea dictumst. Quisque porta tristique turpis",
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas mi nisl, pharetra sit amet nulla nec, lobortis auctor nisl. Proin vestibulum urna nisl, non ultrices nisl malesuada ac. Suspendisse laoreet libero eu neque tristique, eu hendrerit nisl dapibus. Vestibulum aliquet nisl diam, vitae commodo magna egestas dapibus",
     likes: 9,
     image: "https://www.bigstockphoto.com/images/homepage/module-6.jpg"
   },
@@ -29,7 +29,7 @@ export const ARTICLES: Article[] = [
     publish_date: new Date(),
     title: "Lorem 3",
     content:
-      "Nunc a convallis leo. Nunc convallis, odio semper lacinia lacinia, neque dolor pharetra tellus, quis porttitor dui risus id metus. Nullam sed risus eleifend, ornare risus mattis, molestie quam. Cras sed urna at sapien fringilla molestie ac vel leo. Integer et rutrum arcu. Pellentesque mollis nunc at dictum pellentesque. Maecenas porta viverra mauris, quis blandit nunc egestas vulputate. Proin ac sem lorem. In pellentesque molestie rutrum. Sed mollis nec dui eget congue",
+      "Nunc a convallis leo. Nunc convallis, odio semper lacinia lacinia, neque dolor pharetra tellus, quis porttitor dui risus id metus. Nullam sed risus eleifend, ornare risus mattis, molestie quam. Cras sed urna at sapien fringilla molestie ac vel leo. Integer et rutrum arcu",
     likes: 2,
     image:
       "https://image.shutterstock.com/image-photo/colorful-flower-on-dark-tropical-260nw-721703848.jpg"
@@ -40,7 +40,7 @@ export const ARTICLES: Article[] = [
     publish_date: new Date(),
     title: "Lorem 4",
     content:
-      "Integer scelerisque massa at mauris consequat aliquet. Phasellus euismod efficitur erat quis tincidunt. Vivamus ac scelerisque ligula. Praesent bibendum neque elit, iaculis dapibus tellus rutrum at. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nullam nec justo tempor, interdum justo quis, lobortis ex. Integer non erat a ligula eleifend aliquet. Praesent ac velit id risus molestie imperdiet non in nulla. Aenean nec eros lacus. Integer vitae lorem at tortor egestas imperdiet. Curabitur venenatis nisl a velit commodo, et bibendum turpis vulputate. Ut mollis sem nec enim lobortis, ut sagittis velit convallis",
+      "Integer scelerisque massa at mauris consequat aliquet. Phasellus euismod efficitur erat quis tincidunt. Vivamus ac scelerisque ligula. Praesent bibendum neque elit, iaculis dapibus tellus rutrum at. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas",
     likes: 13,
     image:
       "https://image.shutterstock.com/image-photo/beautiful-water-drop-on-dandelion-260nw-789676552.jpg"
@@ -51,7 +51,7 @@ export const ARTICLES: Article[] = [
     publish_date: new Date(),
     title: "Lorem 5",
     content:
-      "Donec tempus nisl a dolor vestibulum, sed rutrum massa ullamcorper. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc laoreet augue et augue viverra tincidunt. Interdum et malesuada fames ac ante ipsum primis in faucibus. Quisque egestas porta enim sed vehicula. Etiam nec tortor semper, vestibulum augue vel, tristique erat. Pellentesque ut lacinia justo. Quisque maximus vitae tellus non sollicitudin. Proin nec cursus velit",
+      "Donec tempus nisl a dolor vestibulum, sed rutrum massa ullamcorper. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc laoreet augue et augue viverra tincidunt. Interdum et malesuada fames ac ante ipsum primis in faucibus. Quisque egestas porta enim sed vehicula",
     likes: 29,
     image:
       "https://image.shutterstock.com/image-photo/white-transparent-leaf-on-mirror-260nw-1029171697.jpg"
@@ -73,7 +73,7 @@ export const ARTICLES: Article[] = [
     publish_date: new Date(),
     title: "Lorem 7",
     content:
-      "The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from Japan. A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was originally bred for hunting",
+      "Sed in sem nulla. Sed vulputate neque in diam gravida, id consectetur dolor ultrices. Morbi ornare magna quis vulputate pretium. Proin semper vel dui vel scelerisque. Fusce hendrerit varius massa, ut facilisis enim aliquet ac. Etiam sed ex felis. Pellentesque rutrum eleifend arcu id auctor. Nullam sagittis ultricies risus in bibendum",
     likes: 60,
     image:
       "https://as.ftcdn.net/r/v1/pics/7b11b8176a3611dbfb25406156a6ef50cd3a5009/home/discover_collections/optimized/image-2019-10-11-11-36-27-681.jpg"
@@ -84,7 +84,7 @@ export const ARTICLES: Article[] = [
     publish_date: new Date(),
     title: "Lorem 8",
     content:
-      "Phasellus venenatis felis fringilla viverra faucibus. Suspendisse interdum ligula odio, non tempus ligula tempor vel. Aliquam ligula lacus, rhoncus nec porttitor ut, vestibulum in libero. Praesent ac ipsum sodales nibh ultrices sollicitudin ac non ligula. Nam sit amet odio justo. Sed in sem nulla. Sed vulputate neque in diam gravida, id consectetur dolor ultrices. Morbi ornare magna quis vulputate pretium. Proin semper vel dui vel scelerisque. Fusce hendrerit varius massa, ut facilisis enim aliquet ac. Etiam sed ex felis. Pellentesque rutrum eleifend arcu id auctor. Nullam sagittis ultricies risus in bibendum",
+      "Phasellus venenatis felis fringilla viverra faucibus. Suspendisse interdum ligula odio, non tempus ligula tempor vel. Aliquam ligula lacus, rhoncus nec porttitor ut, vestibulum in libero. Praesent ac ipsum sodales nibh ultrices sollicitudin ac non ligula. Nam sit amet odio justo",
     likes: 150,
     image: "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg"
   },
@@ -94,7 +94,7 @@ export const ARTICLES: Article[] = [
     publish_date: new Date(),
     title: "Lorem 9",
     content:
-      "Phasellus sit amet placerat neque, eget ornare ante. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla facilisi. Sed dignissim sed tortor non posuere. Aliquam placerat nisi sit amet nisi pellentesque accumsan. Proin id felis eros. Etiam egestas scelerisque nisl, id placerat leo tempus ac. Pellentesque vulputate ultricies est, a pulvinar dui condimentum eget. Donec odio nunc, lobortis aliquam leo in, consectetur mollis velit. Pellentesque non dui vel lacus molestie congue iaculis sed tortor. Suspendisse id ex nec mauris congue gravida. Pellentesque non purus ut nisi dignissim interdum",
+      "Phasellus sit amet placerat neque, eget ornare ante. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla facilisi. Sed dignissim sed tortor non posuere. Aliquam placerat nisi sit amet nisi pellentesque accumsan",
     likes: 140,
     image:
       "https://thumbs.dreamstime.com/b/autumn-oak-leaf-fantastic-beautiful-spray-bubbles-blue-background-magic-autumn-blue-background-yellow-oak-leaf-158238643.jpg"
