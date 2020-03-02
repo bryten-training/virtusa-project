38bcb9a1e33f8c0be959b820f95ba2d1e66701f4
trungvo <vtt311096@gmail.com>
2020-03-02 16:53:01 -0500

    Updated

--- src/app/accounts/accounts.module.ts
+++ src/app/accounts/accounts.module.ts
@@ -5,11 +5,6 @@ import { AccountsRoutingModule } from './accounts-routing.module';
 import { LoginComponent } from './login/login.component';
 import { MaterialModule } from '../material/material.module';
 import { SignUpComponent } from './sign-up/sign-up.component';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSelectModule } from '@angular/material/select';
-import { MatRadioModule } from '@angular/material/radio';
-import { MatCardModule } from '@angular/material/card';
 import { HttpClientModule } from '@angular/common/http';
 
 
