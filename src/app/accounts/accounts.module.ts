829cc3ddba1e4946a98ed60aa0fcce7a9f710507
trungvo <vtt311096@gmail.com>
2020-03-02 17:01:30 -0500

    Updated a bit

--- src/app/accounts/accounts.module.ts
+++ src/app/accounts/accounts.module.ts
@@ -5,8 +5,8 @@ import { AccountsRoutingModule } from './accounts-routing.module';
 import { LoginComponent } from './login/login.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { HttpClientModule } from '@angular/common/http';
+import { MaterialModule } from '../material/material.module';
 
- 
 @NgModule({
   declarations: [
     LoginComponent,
