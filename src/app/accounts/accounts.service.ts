71f7d240bbd0d519ced17142490bfa97e2b71900
Asiwakoti <anjilasiwakoti2053@gmail.com>
2020-03-03 17:24:38 -0500

    yay

--- src/app/accounts/accounts.service.ts
+++ src/app/accounts/accounts.service.ts
@@ -8,56 +8,23 @@ import { tap, share } from 'rxjs/operators';
 export class AccountsService {
 
   loggedInUser: User = null;
-<<<<<<< HEAD
-  obs: Observable<User> = null;
-  observ: Observer<User> = null;
-
-  constructor(private http: HttpClient) {
-  }
-
-  getAllUsers(): Observable<[User]> {
-    return this.http.get<[User]>(`/userdata`);
-  }
-
-  checkUser(user: string, pass: string): Observable<User> {
-    return this.http.get<User>(`/userdata?username=${user}&password=${pass}`).pipe(
-      tap(data => {
-        this.loggedInUser = data;
-      })
-    );
-  }
-
-  signout() {
-    this.loggedInUser = null;
-  }
-=======
   userObs: Observable<User> = null;
   observ: Observer<User>;
 
-  constructor(private http: HttpClient) {}
->>>>>>> b9795af755cf60fb3c4fa17e850d265c1335aa83
+  constructor(private http: HttpClient) { }
 
   getAllUsers(): Observable<[User]> {
     return this.http.get<[User]>(`/Accounts`);
   }
 
-<<<<<<< HEAD
-  private hashPass(pass: string): string {
-    return "A!2cbfgr125!!?7895";
-  }
-
-  getUser(): Observable<User> {
-    return of(this.loggedInUser);
-=======
-  getUser(user:string, pass:string):Observable<User>{
+  getUser(user: string, pass: string): Observable<User> {
     return this.http.get<User>(`/Accounts?username=${user}&password=${pass}`);
   }
 
-  register(user){
+  register(user) {
     console.log(user);
-    
+
     return this.http.post(`http://localhost:3000/Accounts`, user);
->>>>>>> b9795af755cf60fb3c4fa17e850d265c1335aa83
   }
 
 
