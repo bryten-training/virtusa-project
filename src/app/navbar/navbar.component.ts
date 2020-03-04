f0044797d098b325bccc47c7268a5abe3ad2efad
Shengwei Gao <sgao@shengweis-mbp.virtusa.com>
2020-03-04 11:32:03 -0500

    flash card update

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AccountsService } from '../accounts/services/accounts.service';
 import { Auth } from '../accounts/models/auth.model';
+import { User } from '../accounts/models/user.model';
 
 @Component({
   selector: 'app-navbar',
@@ -10,6 +11,7 @@ import { Auth } from '../accounts/models/auth.model';
 export class NavbarComponent implements OnInit {
   isAuthenticated: boolean;
   currentUserName: string;
+  currentUser: User;
 
   constructor(private accountsService: AccountsService) { }
 
@@ -17,7 +19,8 @@ export class NavbarComponent implements OnInit {
     this.accountsService.getBehaviorSubject().subscribe((auth: Auth) => {
       this.isAuthenticated = auth.isAuthenticated;
       this.currentUserName = auth.currentUser ? auth.currentUser.userName : '';
-    })
+      this.currentUser = auth.currentUser;
+    });
   }
 
   onLogOut() {
