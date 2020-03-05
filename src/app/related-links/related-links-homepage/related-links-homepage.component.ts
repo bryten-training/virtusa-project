99e45ec3806c467cbaaf6f7e71bf7a6ec9d9cee5
eason <yul196@pitt.edu>
2020-03-05 14:52:08 -0500

    add relatedlinks list

--- src/app/related-links/related-links-homepage/related-links-homepage.component.ts
+++ src/app/related-links/related-links-homepage/related-links-homepage.component.ts
@@ -11,5 +11,6 @@ export class RelatedLinksHomepageComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  panelOpenState = false;
 
 }
--- src/app/related-links/related-links.module.ts
+++ src/app/related-links/related-links.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RelatedLinksHomepageComponent } from './related-links-homepage/related-links-homepage.component';
+import { MaterialModule } from '../material/material.module';
+import { RouterModule } from '@angular/router';
+import { RelatedLinksRoutingModule } from './relatedlinks-routing.module';
 
 @NgModule({
   declarations: [RelatedLinksHomepageComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialModule,
+    RelatedLinksRoutingModule
   ]
 })
 export class RelatedLinksModule { }
