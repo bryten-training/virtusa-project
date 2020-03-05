16a64bc46dc7fb29e2ba7d8fb5faf38365842e67
trungvo <vtt311096@gmail.com>
2020-03-05 16:09:15 -0500

    Added Login and a bit AccountsService unit tests

--- src/app/accounts/services/accounts.service.spec.ts
+++ src/app/accounts/services/accounts.service.spec.ts
@@ -1,16 +1,51 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AccountsService } from './accounts.service';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { User } from '../models/user.model';
+
 
 describe('AccountsService', () => {
   let service: AccountsService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule, HttpClientTestingModule]
+    });
     service = TestBed.inject(AccountsService);
   });
 
-  it('should be created', () => {
+  it('should AccountsService be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('Login User credentials', () => {
+    service.allUsers = [
+      {
+        userName: 'trungvo',
+        email: 'vtt311096@gmail.com',
+        passWord: 'cGFzcw=='
+      },
+      {
+        userName: 'anish',
+        email: 'anish@gmail.com',
+        passWord: 'cGFzcw=='
+      },
+      {
+        userName: 'yuxuan',
+        email: 'yuxuan@gmail.com',
+        passWord: 'cGFzcw=='
+      }
+    ]
+
+    let user: User = {
+      userName: 'trungvo',
+      email: 'vtt311096@gmail.com',
+      passWord: 'pass'
+    }
+    // expect(service.allUsers.length).toBe(3);
+    expect(service.validateLogIn(user).isValidated).toBe(true);
+  });
+
 });
--- src/app/accounts/services/already-auth.service.spec.ts
+++ src/app/accounts/services/already-auth.service.spec.ts
@@ -1,16 +1,21 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AlreadyAuthService } from './already-auth.service';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AccountsService } from './accounts.service';
 
 describe('AlreadyAuthService', () => {
   let service: AlreadyAuthService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
+      providers: [AccountsService]
+    });
     service = TestBed.inject(AlreadyAuthService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+  // it('should be created', () => {
+  //   expect(service).toBeTruthy();
+  // });
 });
