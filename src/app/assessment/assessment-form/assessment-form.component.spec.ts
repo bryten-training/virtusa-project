f69720f887cc58df517ddb6666d4344bfffe9f75
Shengwei Gao <sgao@Shengweis-MacBook-Pro.local>
2020-03-05 23:14:42 -0500

    last update

--- src/app/assessment/assessment-form/assessment-form.component.spec.ts
+++ src/app/assessment/assessment-form/assessment-form.component.spec.ts
@@ -1,8 +1,5 @@
 // import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-<<<<<<< HEAD
-// import { AssessmentFormComponent } from './assessment-form.component';
-=======
 import { AssessmentFormComponent } from './assessment-form.component';
 import { FormsModule, FormBuilder, ReactiveFormsModule, FormControlName } from '@angular/forms';
 import { By } from '@angular/platform-browser';
@@ -12,20 +9,11 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { DebugElement } from '@angular/core';
 import { MaterialModule } from 'src/app/material/material.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
->>>>>>> e51e0cb2d8532106c60f0a69ba6858712a961aba
 
 // describe('AssessmentFormComponent', () => {
 //   let component: AssessmentFormComponent;
 //   let fixture: ComponentFixture<AssessmentFormComponent>;
 
-<<<<<<< HEAD
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ AssessmentFormComponent ]
-//     })
-//     .compileComponents();
-//   }));
-=======
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       // tslint:disable-next-line: max-line-length
@@ -34,7 +22,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     })
     .compileComponents();
   }));
->>>>>>> e51e0cb2d8532106c60f0a69ba6858712a961aba
 
 //   beforeEach(() => {
 //     fixture = TestBed.createComponent(AssessmentFormComponent);
@@ -42,16 +29,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 //     fixture.detectChanges();
 //   });
 
-<<<<<<< HEAD
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
-=======
   fit('form required', () => {
     const crsEl = fixture.debugElement.nativeElement;
     console.log(crsEl.value);
     expect(crsEl.querySelector('h1').textContent).toContain('Sorry you hvae failed I am sorry try again later');
   });
 });
->>>>>>> e51e0cb2d8532106c60f0a69ba6858712a961aba
